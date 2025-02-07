7efee57005340f55173ecd2f870d270503f74613
ayushukla002 <ayushukla002@gmail.com>
2025-02-07 11:41:17 +0530

    adding login and logout service

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -33,4 +33,39 @@ const signUp = async (req, res) => {
     }
 };
 
-module.exports = signUp;
+
+
+const logIn = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(404).json({ message: "User not found or password is incorrect" });
+        }
+
+        createTokenAndSaveCookie(user._id, res);
+        return res.status(200).json({ message: "User logged in successfully", user });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server error" });
+    }
+};
+
+
+const logOut = (req, res)=>{
+    try {
+        res.clearCookie("jwt")
+        res.status(200).json({message:"user logged out successfully!"})
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server error" });
+    }
+}
+
+
+
+
+module.exports = {signUp, logIn, logOut};
--- backend/routers/user.route.js
+++ backend/routers/user.route.js
@@ -1,8 +1,13 @@
 const express = require("express")
-const signUp = require("../controllers/user.controller.js")
+const {signUp, logIn, logOut} = require("../controllers/user.controller.js")
 
 const router = express.Router()
 
 router.post("/signup", signUp)
+
+router.post("/login", logIn)
+
+router.post("/logout", logOut)
+
 
 module.exports = router
